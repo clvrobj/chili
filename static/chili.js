f7618955fc3eea855c87a4673c42f67b8da0363b
Zhang <clvrobj@gmail.com>
2012-09-30 18:53:43 +0800

    class name confilct

--- static/chili.js
+++ static/chili.js
@@ -1,6 +1,7 @@
 $(function () {
-      $('.repo').each(function (idx, el) {
-                          var user = $(el).attr('data-user'), name = $(el).attr('data-name');
-                          $(el).repo({user:user, name:name}).css('height', '100%');
-                      });
+      $('.github-repo').each(
+          function (idx, el) {
+              var user = $(el).attr('data-user'), name = $(el).attr('data-name');
+              $(el).repo({user:user, name:name}).css('height', '100%');
+          });
   });
