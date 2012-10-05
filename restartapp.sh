ae4604732d5d8a757f1bb41d5798a2806c539791
clvrobj <clvrobj@gmail.com>
2012-10-05 16:18:32 +0800

    start script err log

--- restartapp.sh
+++ restartapp.sh
@@ -7,4 +7,5 @@ if [ -f $PIDFILE ]; then
     rm -f -- $PIDFILE
 fi
 
-exec python ./manage.py runfcgi --protocol=fcgi -h 127.0.0.1 -p 7777 --daemonize --pidfile=/var/run/flaskapp.pid
+exec python ./manage.py runfcgi --protocol=fcgi -h 127.0.0.1 -p 7777 --daemonize --pidfile=/var/run/flaskapp.pid --errlog=/var/chili/logs/flaskerr.log --outlog=/var/chili/logs/flaskout.log
+
