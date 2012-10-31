10462db3a02e391a6f48edc8e5e2c6dcae7bb2bb
Chi <clvrobj@gmail.com>
2012-10-31 23:34:20 +0800

    restart script improve

--- restartapp.sh
+++ restartapp.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-PIDFILE="/var/run/flaskapp.pid"
+PIDFILE="./flaskapp.pid"
+ERRLOG="./flaskerr.log"
+OUTLOG="./flaskout.log"
 
 if [ -f $PIDFILE ]; then
     kill `cat -- $PIDFILE`
     rm -f -- $PIDFILE
 fi
 
-exec python ./manage.py runfcgi --protocol=fcgi -h 127.0.0.1 -p 7777 --daemonize --pidfile=/var/run/flaskapp.pid --errlog=/var/chili/logs/flaskerr.log --outlog=/var/chili/logs/flaskout.log
-
+exec python ./manage.py runfcgi --protocol=fcgi -h 127.0.0.1 -p 7777 --daemonize --pidfile=$PIDFILE --errlog=$ERRLOG --outlog=$OUTLOG
